4d0cd8696ec5ba9c107f7a7f8b3551b34f13f7ce
Angelina <angiestavnk@gmail.com>
2020-05-23 21:33:07 +0300

    Prepare for ts

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+const Landing = () => {
+    return (
+        <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Universal Helper</h1>
+            <p className="lead">
+            Learn statistics. Control the light and fan. Create comfort Conditions.
+            </p>
+            <div className="buttons">
+              <Link to="/register" className="btn btn-primary">Sign Up</Link>
+              <Link to="/login" className="btn btn-light">Login</Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+export default Landing;
